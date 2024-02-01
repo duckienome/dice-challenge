b38f3f18dbbd6dc5859fee89e4b44f35a8c39514
duckie <119513821+duckienome@users.noreply.github.com>
2024-02-02 04:08:41 +0530

    updated all files with roper style

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-// var winner;
-var p1 = Math.floor(Math.random()*7);
-var p2 = Math.floor(Math.random()*7);
-
-// var a1=["first","two","three","four","five","six"]
-// var a2=["ek","dow","teen","chaar","panch","chy"]
+let random_1 = Math.floor(Math.random()*7);
+let random_2 = Math.floor(Math.random()*7);
 
 
+document.querySelector("#random_1").innerText = random_1;
+document.querySelector("#random_2").innerText = random_2;
 
-function winner() {
-    var winner;
-    if (p1>p2) {
-        winner = "P1"
-    } else if(p2>p1) {
-        winner = "P2" 
+
+function decide_winner(){
+    var decide_winner; 
+
+    if (random_1 > random_2) {
+        decide_winner = random_1;
+    } else if (random_1 < random_2) {
+        decide_winner = random_2;
     } else {
-        winner = "Draw"
+        decide_winner = "Draw"
     }
-    outerwinner= winner
+    return decide_winner;
+}
+function show_winner(winner) {
+    if (winner == random_1) {
+        document.querySelector("#random_1").classList.toggle("winner");
+        document.querySelector("#random_2").classList.toggle("loser");
 
-    document.querySelector("h1").innerText = outerwinner;
-    document.querySelector("#p1").innerText = "P1 = "+p1;
-    document.querySelector("#p2").innerText = "P2 = "+p2;
+        document.querySelector("#show_winner").innerText = "random_1 won";
 
-    if (winner == "Draw") {
-        document.querySelector("#p1").classList.toggle("draw");
-        document.querySelector("#p2").classList.toggle("draw");
-    } else if (winner == "P1") {
-        document.querySelector("#p1").classList.toggle("winner");
-        document.querySelector("#p2").classList.toggle("loser");
+    } else if (winner == random_2) {
+        document.querySelector("#random_1").classList.toggle("loser");
+        document.querySelector("#random_2").classList.toggle("winner");
+
+        document.querySelector("#show_winner").innerText = "random_2 won";
+
     } else {
-        document.querySelector("#p1").classList.toggle("loser");
-        document.querySelector("#p2").classList.toggle("winner");
+        document.querySelector("#random_1").classList.toggle("draw");
+        document.querySelector("#random_2").classList.toggle("draw");
+
+        document.querySelector("#show_winner").innerText = "It's a Draw";
+
     }
 }
 
-winner();
-// if (p1>p2) {
-
-// }
+var winner = decide_winner();
+show_winner(winner);
